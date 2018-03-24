30de31ffa4b818448156aeeababd5349f7f30d56
James Titcumb <james@asgrim.com>
2018-03-24 09:09:40 -0400

    Move most content from php_streams2.h to streams2.h as php_streams2.h is used in static builds

--- php_streams2.h
+++ php_streams2.h
@@ -7,17 +7,6 @@
 
 #include "php.h"
 
-#define NS_STREAM "Stream"
-#define NS_STREAM_TRANSPORT "Stream\\Transport"
-#define NS_STREAM_WRAPPER "Stream\\Wrapper"
-
-#define STREAM_SEEK_SET 0
-#define STREAM_SEEK_CUR 1
-#define STREAM_SEEK_END 2
-
-#define PHP_NS_NAMED_FE(ns, name, alias, arginfo) ZEND_NS_NAMED_FE(ns, name, ZEND_FN(alias), arginfo)
-#define PHP_NS_ABSTRACT_ME(ns, classname, name, arginfo) PHP_ABSTRACT_ME(ZEND_NS_NAME(ns, classname), name, arginfo)
-
 extern zend_module_entry streams2_module_entry;
 #define phpext_streams2_ptr &streams2_module_entry
 
--- streams2.h
+++ streams2.h
@@ -5,6 +5,17 @@
 
 PHP_MINIT_FUNCTION(streams2_exceptions);
 
+#define NS_STREAM "Stream"
+#define NS_STREAM_TRANSPORT "Stream\\Transport"
+#define NS_STREAM_WRAPPER "Stream\\Wrapper"
+
+#define STREAM_SEEK_SET 0
+#define STREAM_SEEK_CUR 1
+#define STREAM_SEEK_END 2
+
+#define PHP_NS_NAMED_FE(ns, name, alias, arginfo) ZEND_NS_NAMED_FE(ns, name, ZEND_FN(alias), arginfo)
+#define PHP_NS_ABSTRACT_ME(ns, classname, name, arginfo) PHP_ABSTRACT_ME(ZEND_NS_NAME(ns, classname), name, arginfo)
+
 #ifndef PHP_NS_FE
 #define PHP_NS_FE ZEND_NS_FE
 #endif
